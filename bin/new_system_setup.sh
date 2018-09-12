c73db3117ee22f3ae0964f501a9f035283ec745a
Michele Campeotto <micampe@micampe.it>
2018-09-11 18:17:53 -0700

    updated installed brew packages

--- bin/new_system_setup.sh
+++ bin/new_system_setup.sh
@@ -13,21 +13,22 @@ fi
 PATH=$PATH:~/bin
 
 brew install ack
-brew install ag
 brew install chisel
 brew install exiftool
 brew install fzf
-brew install git
 brew install tree
-brew install vim
+# brew install ag
+# brew install git
+# brew install vim
 
 brew tap caskroom/cask
 
 brew cask install 1password
 brew cask install daisydisk
-brew cask install imageoptim
-brew cask install fork
 brew cask install adobe-creative-cloud
+brew cask install fork
+# brew cask install chromium
+# brew cask install imageoptim
 # brew cask install alfred
 # brew cask install encryptme
 # brew cask install hex-fiend
