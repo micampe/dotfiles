8baf80ba0e60f2189de37f2506e479dc7fb93188
Michele Campeotto <micampe@micampe.it>
2019-10-07 11:19:48 +0200

    fish: changed staged indicator

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -10,8 +10,8 @@ set __fish_git_prompt_color_stagedstate green
 set __fish_git_prompt_color_upstream cyan
 
 # Git Characters
-set __fish_git_prompt_char_dirtystate '●'
-set __fish_git_prompt_char_stagedstate '⇢'
+set __fish_git_prompt_char_dirtystate '● '
+set __fish_git_prompt_char_stagedstate '→'
 set __fish_git_prompt_char_upstream_prefix ' '
 set __fish_git_prompt_char_upstream_equal ''
 set __fish_git_prompt_char_upstream_ahead '⬈'
