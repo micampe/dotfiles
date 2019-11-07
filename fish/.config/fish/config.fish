229ac4cc33df7a094588c4a79405adc8b5a84eeb
Michele Campeotto <micampe@micampe.it>
2019-11-07 10:52:33 +0100

    fish: set the editor variables

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,9 @@
 if status --is-interactive
     set -x PATH ~/bin $PATH
     set -x LESS "-F -g -i -M -R -S -w -X -z-4"
+    set -x EDITOR vim
+    set -x VISUAL vim
+    set -x PAGER less
 
     # cd abbreviations
     abbr --add --global -- - 'cd -'
