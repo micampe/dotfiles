cc31ba99e18c0f6412bb6517350bb44a4fcc9f5d
Michele Campeotto <micampe@micampe.it>
2020-01-30 09:39:30 +0100

    fish: moved to universal variables

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,2 @@
-if status --is-interactive
-    set -x PATH ~/.local/bin $PATH
-    set -x LESS "-F -g -i -M -R -S -w -X -z-4"
-    set -x EDITOR vim
-    set -x VISUAL vim
-    set -x PAGER less
-
-    # cd abbreviations
-    abbr --add --global -- - 'cd -'
-
-    # ls abbreviations
-    abbr --add --global l 'ls'
-    abbr --add --global l1 'ls -1'
-end
+set --export --prepend PATH ~/.local/bin
+set --export --prepend PATH ~/.cargo/bin
