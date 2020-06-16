9b90bbad0a994a315acecde896453c697caed78d
Michele Campeotto <micampe@micampe.it>
2020-06-16 20:39:29 +0200

    set user defaults

--- install.sh
+++ install.sh
@@ -56,3 +56,44 @@ fi
 echo "Updating vim plugins..."
 sleep 1
 vim +PackUpdate +qall
+
+echo "Setting user defaults..."
+
+# Trackpad dragging and tap to click
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+
+# Large mouse pointer
+defaults write com.apple.universalaccess mouseDriverCursorSize -float 2.0
+
+# ctrl-scroll to zoom the screen
+defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
+defaults write com.apple.universalaccess closeViewSmoothImages -bool false
+
+# Hot Corners
+# Lock Screen
+defaults write com.apple.dock wvous-bl-corner -int 13
+defaults write com.apple.dock wvous-bl-modifier -int 0
+# Disable Screen Saver
+defaults write com.apple.dock wvous-tl-corner -int 6
+defaults write com.apple.dock wvous-tl-modifier -int 0
+# Desktop
+defaults write com.apple.dock wvous-br-corner -int 4
+defaults write com.apple.dock wvous-br-modifier -int 0
+# Mission Control
+defaults write com.apple.dock wvous-tr-corner -int 2
+defaults write com.apple.dock wvous-tr-modifier -int 0
+
+# Dock
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock tilesize -int 64
+
+# Spaces
+defaults write com.apple.dock mru-spaces -bool false
+defaults write com.apple.dock expose-group-apps -bool true
+defaults write -g AppleSpacesSwitchOnActivate -bool false
+
+# Keyboard Equivalents
+defaults write -g NSUserKeyEquivalents -dict-add Zoom "@^z"
+
+echo "Some defaults require the session to be restarted."
