e331752f20e11497e6a66ef083e9d1fd4b1e7b1b
Michele Campeotto <micampe@micampe.it>
2020-08-20 11:01:21 +0200

    brew: install casks in home directory

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,5 @@
 set --export --prepend PATH ~/.local/bin
 set --export --prepend PATH ~/.cargo/bin
 set --export --prepend PATH ~/.gem/ruby/*/bin
+
+set --export HOMEBREW_CASK_OPTS "--appdir=/Applications"
