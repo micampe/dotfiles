df023e599b7101b05761db2d19b3ec631dd5975e
Michele Campeotto <micampe@micampe.it>
2021-09-14 12:24:05 +0200

    fixed fish python paths

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
-fish_add_path ~/Library/Python/3.9/bin
+fish_add_path ~/Library/Python/*/bin
 fish_add_path ~/.gem/ruby/*/bin
 
 set --export HOMEBREW_CASK_OPTS "--appdir=/Applications"
