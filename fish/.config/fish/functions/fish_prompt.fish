15e779997eb8db93d73193fe563f3d8229fbc25d
Michele Campeotto <micampe@micampe.it>
2022-06-17 17:20:31 +0200

    fish: simplified prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,60 +1,42 @@
 # https://github.com/brandonweiss/pure.fish
-set fish_prompt_pwd_dir_length 0
 
 # Git prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_color_branch 242
-set __fish_git_prompt_color_dirtystate FCBC47
+set __fish_git_prompt_color_dirtystate yellow
 set __fish_git_prompt_color_stagedstate green
 set __fish_git_prompt_color_upstream cyan
 
 # Git Characters
 set __fish_git_prompt_char_dirtystate '● '
 set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_upstream_prefix ' '
 set __fish_git_prompt_char_upstream_equal ''
 set __fish_git_prompt_char_upstream_ahead '⬈'
 set __fish_git_prompt_char_upstream_behind '⬋'
 set __fish_git_prompt_char_upstream_diverged '⬋⬈'
 
-function _print_in_color
-  set -l string $argv[1]
-  set -l color  $argv[2]
-
-  set_color $color
-  printf $string
-  set_color normal
-end
-
-function _prompt_for_status
-  if test $argv[1] -eq 0
-    echo '❯ '
-  else
-    echo '✕ '
-  end
-end
-
-function _prompt_color_for_status
-  if test $argv[1] -eq 0
-    echo magenta
-  else
-    echo brred
-  end
-end
-
 function fish_prompt
   set -l last_status $status
+  set -l normal (set_color normal)
+  set -l cwd_color (set_color blue)
+  set -l status_color (set_color magenta)
+  set -l login_color (set_color yellow)
+  set -l suffix '❯ '
+  set -l prefix '🐙 '
 
-  _print_in_color "\n"(prompt_pwd) blue
+  set -lx fish_prompt_pwd_dir_length 0
 
-  __fish_git_prompt " %s"
-
-  if test "$SSH_CLIENT" = ""
-    printf '\n🐙 '
-  else
-    _print_in_color '\n'(whoami)'@'(hostname)' ' yellow
+  if set -q SSH_TTY
+    set prefix $login_color (whoami) '@' (hostname) ' '
   end
 
-  _print_in_color (_prompt_for_status $last_status) (_prompt_color_for_status $last_status)
+  if test $last_status -ne 0
+    set status_color (set_color brred)
+    set suffix '✕ '
+  end
+
+  echo
+  echo -s $cwd_color (prompt_pwd) (fish_vcs_prompt ' %s')
+  echo -s -n $prefix $status_color $suffix $normal
 end
