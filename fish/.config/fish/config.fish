dddd10df82fb490a33be1da9588f5c6c2763e510
Michele Campeotto <micampe@micampe.it>
2022-08-25 13:58:02 +0200

    fish: path is in the universal variable

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,3 @@
-fish_add_path --prepend --move /opt/homebrew/bin
-fish_add_path --prepend --move ~/.local/bin
-fish_add_path --prepend --move ~/.cargo/bin
-
 if command -q brew
     set --export HOMEBREW_CASK_OPTS "--appdir=/Applications"
 end
