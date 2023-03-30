1d516b761bf9aaef4efebe520f1662fe572fd86c
Michele Campeotto <micampe@micampe.it>
2023-03-30 11:54:56 +0200

    fish: moved to new abbreviations

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+abbr -a -- extract 'atool --extract --explain' # imported from a universal variable, see `help abbr`
+abbr -a -- lla 'ls -oAh' # imported from a universal variable, see `help abbr`
+abbr -a -- la 'ls -A' # imported from a universal variable, see `help abbr`
+abbr -a -- - 'cd -' # imported from a universal variable, see `help abbr`
+abbr -a -- l ls # imported from a universal variable, see `help abbr`
+abbr -a -- l1 'ls -1' # imported from a universal variable, see `help abbr`
+abbr -a -- ll 'ls -oh' # imported from a universal variable, see `help abbr`
