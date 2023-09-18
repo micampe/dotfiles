203bb0c36c85f93463f2ad4fffc90ca457039ea2
Michele Campeotto <micampe@micampe.it>
2023-09-18 09:20:06 +0200

    fixed fish_title

--- .config/fish/functions/fish_title.fish
+++ .config/fish/functions/fish_title.fish
@@ -3,7 +3,7 @@ function fish_title
   set -l max_title_len 30
   if test (status current-command) = 'fish'
     echo (prompt_pwd)
-  else if test (string length $argv) -gt 30 #$max_title_len
+  else if test "(string length $argv) -gt $max_title_len"
     # why can't I use the variable here?
     set -l ellipsis \u2026
     set -l title (string trim (string sub -l $max_title_len $argv))$ellipsis
