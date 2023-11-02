31a2d662e832221b03dfdde6b7b2d4c47719b590
Michele Campeotto <micampe@micampe.it>
2023-11-02 15:27:29 +0100

    ac: updated script paths

--- .config/fish/functions/ac-living-room.fish
+++ .config/fish/functions/ac-living-room.fish
@@ -1,3 +1,3 @@
 function ac-living-room --wraps='python ~/src/ewpe/gree-remote/PythonCLI/gree.py -c 10.103.1.149 -i 502cc69b8a3f -k X0Lt2h7W1LsZh8v3' --description 'control living room AC'
-  python3 ~/src/ewpe/gree-remote/PythonCLI/gree.py -c 10.103.1.149 -i 502cc69b8a3f -k X0Lt2h7W1LsZh8v3 $argv; 
+  python3 ~/src/ac-gree-remote/gree-remote/PythonCLI/gree.py -c 10.103.1.149 -i 502cc69b8a3f -k X0Lt2h7W1LsZh8v3 $argv; 
 end
--- .config/fish/functions/ac-scan.fish
+++ .config/fish/functions/ac-scan.fish
@@ -1,3 +1,3 @@
 function ac-scan --wraps='python ~/src/ewpe/gree-remote/PythonCLI/gree.py -b 10.103.1.255 search' --description 'scan the network for AC units'
-  python3 ~/src/ewpe/gree-remote/PythonCLI/gree.py -b 10.103.1.255 search $argv; 
+  python3 ~/src/ac-gree-remote/gree-remote/PythonCLI/gree.py -b 10.103.1.255 search $argv; 
 end
