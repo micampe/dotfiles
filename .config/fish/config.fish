d6b72485b2137eb537fb7633ffcb1aa89ffb2f7f
Michele Campeotto <micampe@micampe.it>
2024-04-03 11:14:25 +0200

    fish: moved shell variables to config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,28 @@ abbr -a -- l1 'ls -1'
 abbr -a -- la 'ls -A'
 abbr -a -- ll 'ls -oh'
 abbr -a -- lla 'ls -oAh'
+
+set --export COLORTERM "1"
+set --export EDITOR "vim"
+set --export HOMEBREW_AUTOREMOVE "1"
+set --export HOMEBREW_CASK_OPTS "--appdir=~/Applications/"
+set --export HOMEBREW_DISPLAY_INSTALL_TIMES "1"
+set --export HOMEBREW_NO_ENV_HINTS "1"
+set --export LC_ALL "en_US.UTF-8"
+set --export LESS "--hilite-unread --window=-4 --quit-if-one-screen --hilite-search --ignore-case --LONG-PROMPT --RAW-CONTROL-CHARS --no-init"
+set --export PAGER "less"
+set --export VISUAL "vim"
+
+# XDG
+set --export XDG_CACHE_HOME "$HOME/.cache"
+set --export XDG_CONFIG_HOME "$HOME/.config"
+set --export XDG_DATA_HOME "$HOME/.local/share"
+set --export XDG_STATE_HOME "$HOME/.local/state"
+
+set --export CARGO_HOME "$XDG_DATA_HOME/cargo"
+set --export DOCKER_CONFIG "$XDG_CONFIG_HOME/docker"
+set --export HISTFILE "$XDG_STATE_HOME/bash/history"
+set --export LESSHISTFILE "$XDG_STATE_HOME/less/history"
+set --export PYENV_ROOT "$XDG_DATA_HOME/pyenv"
+set --export RUSTUP_HOME "$XDG_DATA_HOME/rustup"
+set --export SQLITE_HISTORY "$XDG_CACHE_HOME/sqlite_history"
